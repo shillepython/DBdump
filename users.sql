88f0efc659a1bb9e296d58fd9d25cfc48539ba8e
shillepython <shillenetwork@gmail.com>
2019-10-02 02:36:13 +0300

    v2 table

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 01 2019 г., 21:54
+-- Время создания: Окт 02 2019 г., 02:35
 -- Версия сервера: 5.7.25
 -- Версия PHP: 7.1.22
 
@@ -31,13 +31,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
-  `name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `surname` varchar(255) NOT NULL,
   `birth_date` varchar(255) NOT NULL,
-  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) NOT NULL,
   `tel` int(11) NOT NULL,
-  `registration_date` datetime NOT NULL,
-  `group_id` varchar(255) NOT NULL,
+  `registration_date` date NOT NULL,
+  `group_id` varchar(255) NOT NULL DEFAULT 'Отсутствует',
   `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -45,9 +46,11 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `name`, `surname`, `birth_date`, `email`, `tel`, `registration_date`, `group_id`, `role_id`) VALUES
-(1, 'vasja', 'ivanov', 'ivan', '06.07.2004', 'vasja@gmail.com', 0, '2019-07-21 12:33:33', 'JS-PHP-1', 1),
-(2, 'petja', 'petja', 'surdgik', '25.03.1993', 'petja@gmail.com', 0, '2019-09-21 12:33:33', 'JS-PHP-2', 2);
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `surname`, `birth_date`, `email`, `tel`, `registration_date`, `group_id`, `role_id`) VALUES
+(1, 'vasja', '', 'ivanov', 'ivan', '06.07.2004', 'vasja@gmail.com', 0, '2019-07-21', 'JS-PHP-1', 1),
+(2, 'petja', '', 'petja', 'surdgik', '25.03.1993', 'petja@gmail.com', 0, '2019-09-21', 'JS-PHP-2', 2),
+(65, 'Semikhat', '123', 'Simavarvar', 'Semikhat', '02.03.2000', 'serafim123semikhat@gmail.com', 980193160, '2010-02-19', 'Отсутствует', 1),
+(73, 'Semikhat123', '123', 'Simavarvar', 'Semikhat', '02.03.2000', 'serafim123semikhat@gmail.com', 980193160, '2010-02-19', 'Отсутствует', 1);
 
 --
 -- Индексы сохранённых таблиц
@@ -69,7 +72,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
